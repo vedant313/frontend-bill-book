0733bb67df8bc17d5293d449065fd66fab7a3b85
vedant313 <vedantb150@gmail.com>
2026-08-09 14:59:19 +0530

    updete

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://backend-fwio.onrender.com/api",
         changeOrigin: true,
       },
     },
