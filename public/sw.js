889bad17bf0e721c6351bab3cfd2a8caf894c9e0
Vedant Bhavsar <vedantb150@gmail.com>
2026-09-19 20:02:26 +0530

    fix: refresh Nexsa Bill service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="billbook-shell-v1";
+const CACHE="nexsa-bill-shell-v2";
 const APP_SHELL=["/","/index.html","/manifest.webmanifest","/icons/icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
